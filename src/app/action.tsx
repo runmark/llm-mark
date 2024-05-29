02b8f9d900e73b8f8e9e0e0621ee8bdc683a9616
maxingmin <ma_xingmin@163.com>
2024-05-29 03:22:16 +0000

    ✨ feat(action): fetch contents of links

--- src/app/action.tsx
+++ src/app/action.tsx
@@ -4,6 +4,10 @@ import { OllamaEmbeddings } from "@langchain/community/embeddings/ollama";
 import { OpenAIEmbeddings } from '@langchain/openai';
 import { OpenAI } from 'openai';
 import { config } from './config';
+import cheerio from "cheerio";
+import { TIMEOUT } from 'dns';
+import { ChevronLeftIcon } from '@radix-ui/react-icons';
+import { error } from 'console';
 // import { functionCalling } from './function-calling';
 // OPTIONAL: Use Upstash rate limiting to limit the number of requests per user
 
@@ -55,8 +59,91 @@ interface ContentResult extends SearchResult {
 }
 
 // 2. fetch search results from Brave Search API
+export async function getSources(message: string, numberOfPagesToScan = config.numberOfPagesToScan): Promise<SearchResult[]> {
+    try {
+        const response = await fetch(`https://api.search.brave.com/res/v1/web/search?q=${encodeURIComponent(message)}&count=${numberOfPagesToScan}`, {
+            headers: {
+                'Accept': 'application/json',
+                'Accept-Encoding': 'gzip',
+                'X-Subscription-Token': process.env.BRAVE_SEARCH_API_KEY as string
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
-// 3. fetch contents of top10 search results
+        const jsonResponse = await response.json();
+        if (!jsonResponse.web || !jsonResponse.web.results) {
+            throw new Error("Invalid API response format");
+        }
+
+        const final = jsonResponse.web.results.map((result: any): SearchResult => ({
+            title: result.title,
+            link: result.url,
+            favicon: result.profile.img,
+        }));
+
+        return final;
+    } catch (err) {
+        console.log("Error fetching search result: ", err);
+        throw err;
+    }
+}
+
+// 3. fetch contents of search results
+export const getBlueLinksContent = async (sources: SearchResult[]): Promise<ContentResult[]> => {
+    const fetchWithTimeout = async (url: string, options: RequestInit = {}, timeout = 800): Promise<Response> => {
+        try {
+            const controller = new AbortController();
+            const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+            const response = await fetch(url, { ...options, signal: controller.signal });
+            clearTimeout(timeoutId);
+
+            return response;
+        } catch (err) {
+            if (err) {
+                console.log(`skipping ${url}!`);
+            }
+            throw err;
+        }
+    };
+
+    // b.parse and extract needful content
+    const extractMainContent = (html: string): string => {
+        try {
+            const $ = cheerio.load(html);
+            $("script, style, head, footer, nav, iframe, img").remove();
+
+            return $("body").text().replace(/\s+/g, " ").trim();
+        } catch (err) {
+            console.log("Error extracting main content: ", err);
+            throw error;
+        }
+    };
+
+    const contentPromises = sources.map(async (source): Promise<ContentResult | null> => {
+        try {
+            const response = await fetchWithTimeout(source.link, {}, 800);
+            if (!response.ok) {
+                throw new Error(`Failed to fetch ${source.link}. Status: ${response.status}`);
+            }
+
+            const mainContent = extractMainContent(await response.text());
+            return { ...source, html: mainContent };
+        } catch (err) {
+            return null;
+        }
+    });
+
+    try {
+        const results = await Promise.all(contentPromises);
+        return results.filter((content): content is ContentResult => content !== null);
+    } catch (err) {
+        console.log(`Error fetching and processing blue links contents: `, err);
+        throw err;
+    }
+}
 
 // 4. process and vectorize content using Langchain
 
